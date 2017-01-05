22c8f19818fa1e3a16e75e517616d96547358109
THUAN\cowbo <cowboy9x01@hotmail.com>
2017-01-05 22:04:24 +0700

    add domain entities for project

---
+++ TeduShop.Model/Models/Post.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "varchar")]
+        public string Alias { get; set; }
+
+        [Required]
+        public int CategoriesID { get; set; }
+
+        [MaxLength(256)]
+        public string Image { get; set; }
+
+        public string Content { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/VisistorStatistic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    public class VisistorStatistic
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public DateTime VisitedDate { get; set; }
+
+        [MaxLength(50)]
+        public string IPAddress { get; set; }
+    }
+}
